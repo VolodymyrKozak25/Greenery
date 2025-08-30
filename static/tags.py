60b24f272b79a200668822dfc3773d0370f48e8c
VolodymyrKozak25 <volodymyrkozak2545@gmail.com>
2025-08-30 12:42:54 +0300

    Adding new tags

--- static/tags.py
+++ static/tags.py
@@ -1,7 +1,8 @@
 LINESTRING_WIDTH = 1
 POINT_AREA = 1
 
-TAG_GROUPS = ('leisure', 'natural', 'landuse')
+TAG_GROUPS = ('leisure', 'natural', 'landuse', 
+              'place', 'landcover', 'barrier')
 
 TAGS = {
     'Парк': ('leisure', 'park'),
@@ -9,7 +10,18 @@ TAGS = {
     'Місце для пікніка зі столиком': ('leisure', 'picnic_table'),
     'Природний заповідник': ('leisure', 'nature_reserve'),
     'Кущі': ('natural', 'shrubbery'),
+    'Чагарники': ('natural', 'scrub'),
+    'Окреме дерево': ('natural', 'tree'),
     'Ряд дерев': ('natural', 'tree_row'),
+    'Група дерев': ('landcover', 'trees'),
+    'Ліс (природний)': ('natural', 'wood'),
+    'Лісогосподарство': ('landuse', 'forest'),
     'Трав\'яне покриття': ('landuse', 'grass'),
+    'Трав\'яна територія': ('landcover', 'grass'),
     'Клумба': ('landuse', 'flowerbed'),
+    'Луг, пасовище': ('landuse', 'meadow'),
+    'Плодовий сад': ('landuse', 'orchard'),
+    'Виноградник': ('landuse', 'vineyard'),
+    'Газон': ('place', 'green'),
+    'Живопліт': ('barrier', 'hedge'),
 }
